f10c571d9cbd31884a5c26e16fa6edebd0bdd7d5
Christine Jeon <csjeon28@gmail.com>
2021-04-11 20:48:32 -0400

    commit tree e4d72de47cf6649988716dd94aa7a05fa0c5eef7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+function findMatching (collection, name) {
+     let matchedValues = [];
+     let count = 0;
+     for (const driver of collection) {
+         if (driver.toLowerCase() === name.toLowerCase()) {
+             matchedValues[count] = driver;
+             count++;
+         } 
+     } 
+     return matchedValues;
+}
+
+function fuzzyMatch (collection, begName) {
+    let matchedValues = [];
+    let count = 0;
+    let begNameLength = begName.length;
+    for (const driver of collection) {
+        if (driver.slice(0,begNameLength) === begName) {
+            matchedValues[count] = driver;
+            count++;
+        } 
+    } 
+    return matchedValues;
+}
+
+function matchName (collection, driverName) {
+    let matchedValues = [];
+    let count = 0;
+    for (const profile of collection) {
+        if (profile.name === driverName) {
+            matchedValues[count] = profile;
+            count++;
+        } 
+    } 
+    return matchedValues;
+}
